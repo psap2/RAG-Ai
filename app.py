8fcdc7846d8ef785d75e5544400cff16791efbf9
Prasun Sapkota <prasun1@unc.edu>
2025-01-21 14:00:33 -0500

    Inseting embeddings into Vectordatabase

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ chroma_client, collection, google_ef, collection_name = initialize_config()
 #load articles
 directory = "./news_articles"
 documents = load_documents_from_directory(directory)
-print(len(documents))
 
 #create chunks for articles
 chunked_documents = []
@@ -16,3 +15,12 @@ for document in documents:
     for i, chunk in enumerate(chunks):
         chunked_documents.append({"id": f"{document['id']}_chunk{i+1}", "text": chunk})
 
+#getting embedding for documents broken into chunks
+for chunk in chunked_documents:
+    chunk["embedding"] = create_embedding(chunk["text"])
+
+# Upsert documents with embeddings into Chroma vector database
+for chunk in chunked_documents:
+    collection.upsert(
+        ids=[chunk["id"]], documents=[chunk["text"]], embeddings=[chunk["embedding"]]
+    )
